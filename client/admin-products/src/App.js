218eb19079fb12e258a674e77650b4a7a46c8823
Augusto Baez <augustobaez@iMac-de-abc-desarrollo.local>
2023-01-31 14:54:45 -0300

    post and get working with id too

--- client/admin-products/src/App.js
+++ client/admin-products/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Form from './components/Form';
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./components/Form";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <ProductList />
     </div>
   );
 }
---
+++ client/admin-products/src/components/ProductList.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+const ProductList = () => {
+  const [productos, setProductos] = useState([]);
+  const [productId, setProductId] = useState([]);
+
+  useEffect(() => {
+    const getProductos = async () => {
+      try {
+        const result = await axios.get("http://localhost:8000/api/products");
+        setProductos(result.data.product.map((product) => product));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProductos();
+  }, []);
+
+  console.log(productId);
+  return (
+    <div>
+      <h2>All Products</h2>
+      {productos.map((prod) => (
+        <>
+          <h2>
+            <a href={`/product/${prod._id}`}>{prod.title}</a>
+          </h2>
+          {/*           <h2>{prod._id}</h2> */}
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
