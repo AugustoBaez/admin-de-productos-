fadc0a438a7bd714bab3c1f6d84921bdc2452f60
Augusto Baez <augustobaez@iMac-de-abc-desarrollo.local>
2023-01-31 17:47:51 -0300

    cambios

--- client/admin-products/src/components/ProductList.js
+++ client/admin-products/src/components/ProductList.js
@@ -1,21 +1,18 @@
 import React from "react";
 import axios from "axios";
 export default (props) => {
-  console.log(props.people.product);
-
-  let data = props.people;
   return (
     <div>
       <h2>All Products:</h2>
-      <h3>{props.people}</h3>
-      {/* {props.people.map((person, idx) => {
+      {/*       <h3>{props.people}</h3> */}
+      {props.products.map((producto, idx) => {
         <>
           <p key={idx}>
-            {person.lastName}, {person.firstName}
+            {producto.title}, {producto.price}, {producto.description}
           </p>
           ;
         </>;
-      })} */}
+      })}
     </div>
   );
 };
