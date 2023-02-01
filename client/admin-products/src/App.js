37e857e1a031595b11629dde80685a9b41d4aa5e
asbae <asbaez@outlook.com>
2023-01-31 21:41:21 -0300

    comments

--- client/admin-products/src/App.js
+++ client/admin-products/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Form from "./components/Form";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; //usa el latest para funcionar
 import Product from "./components/Product";
 function App() {
   return (
--- client/admin-products/src/components/Product.jsx
+++ client/admin-products/src/components/Product.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import axios from "axios"
+import { useParams } from 'react-router-dom' //usa el latest router para funcionar
+import axios from "axios" 
 const Product = () => {
-    const { id } = useParams()
+    const { id } = useParams() //con params trae el id de la uri
     const [specProduct, setSpecProduct] = useState([])
     const [prodTitle, setProdTitle] = useState("")
     const [prodPrice, setProdPrice] = useState()
@@ -11,7 +11,7 @@ const Product = () => {
     useEffect(() => {
         const getProducto = async () => {
             try {
-                const result = await axios.get(`http://localhost:8000/api/products/${id}`)
+                const result = await axios.get(`http://localhost:8000/api/products/${id}`) //se pone el id del useparams aca para hacer fetch de esa id
                 setProdTitle(result.data.product.map((prod) => prod.title))
                 setProdPrice(result.data.product.map((prod) => prod.price))
                 setProdDesc(result.data.product.map((prod) => prod.description))
