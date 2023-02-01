e0848c6bd8052c9a03bc431b00679c576967a654
asbae <asbaez@outlook.com>
2023-01-31 23:12:14 -0300

    error

--- client/admin-products/src/components/Product.jsx
+++ client/admin-products/src/components/Product.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom' //usa el latest router para funcionar
-import axios from "axios" 
+import axios from "axios"
+import { useNavigate } from 'react-router-dom'
 const Product = () => {
     const { id } = useParams() //con params trae el id de la uri
     const [specProduct, setSpecProduct] = useState([])
     const [prodTitle, setProdTitle] = useState("")
     const [prodPrice, setProdPrice] = useState()
     const [prodDesc, setProdDesc] = useState([])
+    const navigate = useNavigate()
 
     useEffect(() => {
         const getProducto = async () => {
@@ -25,12 +27,24 @@ const Product = () => {
 
     console.log(specProduct)
 
+    const deleteProduct = async () => {
+         try {
+                const result = await axios.delete(`http://localhost:8000/api/products/${id}`) //se pone el id del useparams aca para hacer fetch de esa id
+                .then((res) => {
+                    navigate("/")
+                })
+
+            } catch (error) {
+                console.log(error)
+            }
+    }
+
     return (
         <div>
             <h2>{prodTitle}</h2>
             <h3>Price: ${prodPrice}</h3>
             <h3>{prodDesc}</h3>
-            
+            <button onClick={deleteProduct}>Delete</button>
         </div>
     )
 }
--- client/admin-products/src/components/ProductList.jsx
+++ client/admin-products/src/components/ProductList.jsx
@@ -1,15 +1,18 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-
+// import { useNavigate } from 'react-router-dom'
 const ProductList = () => {
   const [productos, setProductos] = useState([]);
-  const [productId, setProductId] = useState([]);
+  // const [prodId, setProdId] = useState([]);
+  // const navigate = useNavigate()
+
 
   useEffect(() => {
     const getProductos = async () => {
       try {
         const result = await axios.get("http://localhost:8000/api/products/");
         setProductos(result.data.product.map((product) => product));
+        // setProdId(result.data.product.map((product) => product._id));
       } catch (error) {
         console.log(error);
       }
@@ -17,16 +20,30 @@ const ProductList = () => {
     getProductos();
   }, []);
 
-  console.log(productId);
+  const removeFromDom = productId => {
+        setProductos(productos.filter(prod => prod._id !== productId));
+    }
+
+  // const deleteProduct = (prodId) => {
+  //   axios.delete(`http://localhost/api/products/delete/${prodId}`)
+  //   .then(res => {
+  //     navigate("/")
+  //   }).catch((err) => {
+  //     console.log(err)
+  //   })
+  // }
+
   return (
     <div>
       <h2>All Products</h2>
       {productos.map((prod) => (
         <>
+        <div className="products">
           <h2>
             <a href={`/${prod._id}`}>{prod.title}</a>
           </h2>
-          {/*           <h2>{prod._id}</h2> */}
+          {/* <button onClick={() => deleteProduct(prod._id)}>Delete</button> */}
+        </div>
         </>
       ))}
     </div>
